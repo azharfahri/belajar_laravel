36f3850c528a52de892d809d976fcb1539156e2c
azharfahri <fahrikatepp@gmail.com>
2025-04-14 16:46:22 +0700

    templating

---
+++ resources/views/layouts/part/sidebar.blade.php
@@ -0,0 +1,29 @@
+<aside class="sidebar navbar-default" role="navigation">
+                
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li class="sidebar-search">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+                <!-- /input-group -->
+            </li>
+            <li>
+                <a href="index.html" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+            </li>
+            
+            <li>
+                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+            </li>
+            
+            
+            
+        </ul>
+    </div>
+    
+</aside>
